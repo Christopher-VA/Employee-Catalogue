73c73856992529bc45a90bcef2b2907e745e726a
Christopher-VA <cvonaltenstadt@gmail.com>
2024-01-15 15:00:11 +0800

    inquirer prompts start

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const inquirer = require('inquirer');
+const mysql = require("mysql2");
 
-const questions = [{
-    type: 'list',
-    name: 'question',
-    message: 'What would you like to do?',
-    option: ['Add Employee', 'Delete Employee', 'Add Department', 'Delete Department', 'Add Role', 'Delete Role', 'Update Employee Role', 'Update Employee Department']
+function init() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "command",
+            message: "What would you like to do?",
+            option: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role", "quit"],
+        })
+        .then((answer) => {
+            switch (answer.command) {
+                case "view all departments":
+                    departments();
+                    break;
+            }
+        })
 }
-];
+
+function departments() {
+    const query = "SELECT * FROM departments";
+    console.log(query);
+    init();
+}
+
+init();
